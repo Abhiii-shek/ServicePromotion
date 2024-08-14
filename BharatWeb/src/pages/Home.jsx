00b68cbf7ae320da8ff251425dd5720c8eb90cfe
Abhishek rajbhar <rajbhar.abhishek84912@gmail.com>
2024-08-15 03:17:09 +0530

    fixed size

--- BharatWeb/src/pages/Home.jsx
+++ BharatWeb/src/pages/Home.jsx
@@ -13,11 +13,11 @@ function Home() {
   return (
     <div>
       <section 
-      className="bg-[url('/src/assets/images/Hero.jpg')] bg-contain h-[50vh] bg-no-repeat md:bg-cover text-white md:h-screen p-20 text-center  border-b border-zinc-900">
+      className="bg-[url('/src/assets/images/Hero.jpg')] bg-contain  bg-no-repeat md:bg-cover text-white md:h-screen p-20 text-center  border-b border-zinc-900">
         <img src={""} alt="" />
         <h1 className="md:text-5xl text-4xl  lg:text-6xl font-bold mb-4 mt-12  md:mt-20 text-[#5b2fdf]">Boost Your Business with Our Services</h1>
         <p className="text-xl lg:text-3xl md:text-2xl mb-8">Web Development, SEO, and Digital Marketing Experts</p>
-        <a href="/services" className="bg-white  text-2xl px-6 py-3 font-bold rounded-full text-[#5b2fdf]">Get Started</a>
+        <button href="/services" className="bg-white  text-2xl px-6 py-3 font-bold rounded-full text-[#5b2fdf]">Get Started</button>
       </section>
       <section>
         <OurServices/>
